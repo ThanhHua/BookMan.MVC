9f0e3740f3841cec2e686500a53a47c244d0fd3d
Hua Thanh <thanhhua17011997@gmail.com>
2020-10-28 21:41:06 +0700

    Add Delete function

--- BookMan.Mvc/Controllers/BookController.cs
+++ BookMan.Mvc/Controllers/BookController.cs
@@ -21,6 +21,21 @@ namespace BookMan.Mvc.Controllers
             if (b == null) return NotFound();
             else return View(b);
         }
+
+        public IActionResult Delete(int id)
+        {
+            var b = _service.Get(id);
+            if (b == null) return NotFound();
+            else return View(b);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Book book)
+        {
+            _service.Delete(book.Id);
+            _service.SaveChanges();
+            return RedirectToAction("Index");
+        }
        
     }
 }
