4e38069ccc850c3722c5e0ca6c6c110dfad71443
Hua Thanh <thanhhua17011997@gmail.com>
2020-10-28 21:09:22 +0700

    Add project files.

---
+++ BookMan.Mvc/Controllers/BookController.cs
@@ -0,0 +1,26 @@
+﻿using BookMan.Mvc.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookMan.Mvc.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly Service _service;
+        public BookController(Service service)
+        {
+            _service = service;
+        }
+        public IActionResult Index()
+        {
+            return View(_service.Get());
+        }
+
+        public IActionResult Details(int id)
+        {
+            var b = _service.Get(id);
+            if (b == null) return NotFound();
+            else return View(b);
+        }
+       
+    }
+}
